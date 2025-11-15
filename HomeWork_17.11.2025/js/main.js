84f6e304b8975926f4aeae10d1ec5a05377bc9a5
levtoshi <Intel@SATOSHI-LAPTOP>
2025-11-15 19:27:17 +0200

    Add HW 17.11.2025

---
+++ HomeWork_17.11.2025/js/main.js
@@ -0,0 +1,61 @@
+class Button {
+    constructor(text, border, color, background) {
+        this.text = text;
+        this.border = border;
+        this.color = color;
+        this.background = background;
+    }
+
+    showInfo() {
+        console.log(`Button "${this.text}" clicked. Button color - ${this.background}`);
+    }
+
+    render() {
+        this.element = document.createElement("button");
+        const container = document.querySelector(".container");
+
+        this.element.classList.add("click-btn");
+
+        this.element.textContent = this.text;
+        this.element.style.border = this.border;
+        this.element.style.color = this.color;
+        this.element.style.background = this.background;
+
+        this.element.addEventListener("click", () => this.showInfo());
+
+        container.append(this.element);
+    }
+}
+
+class RoundedButton extends Button
+{
+    constructor (text, border, color, background, borderRadius)
+    {
+        super(text, border, color, background);
+        this.borderRadius = borderRadius;
+    }
+
+    render()
+    {
+        super.render();
+        this.element.style.borderRadius = `${this.borderRadius}px`;
+    }
+
+    dropShadow()
+    {
+        this.element.style.borderRadius = `0px 5px 5px red`;
+    }
+}
+
+const buttons = [
+    new Button("Click me", "3px solid black", "white", "blue"),
+    new Button("Press here", "3px solid orange", "white", "green"),
+    new Button("Click here", "3px solid blue", "black", "white"),
+    new RoundedButton("Rounded_1", "3px solid green", "green", "black", 10),
+    new RoundedButton("Rounded_2", "3px solid purple", "white", "blue", 20),
+    new RoundedButton("Rounded_3", "3px solid magenta", "white", "orange", 30),
+];
+
+for (const button of buttons) {
+    button.render();
+}
